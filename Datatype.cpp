77d0f3c3f3558788d18314d276070665191661f0
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2024-02-12 22:06:18 +0600

    Add files via upload

---
+++ Datatype.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1 = 10;
+    int num2 = 12;
+    int sum = num1+num2;
+
+    cout<<"Number "<<num1<<" and number "<<num2<<" = "<<sum;
+    return 0;
+}
