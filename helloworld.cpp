4d8ea5390fe417e46c781b7c6ae914ffc82d2bb1
Delowar Husain <138960860+delowarshizar@users.noreply.github.com>
2024-02-12 22:05:29 +0600

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Delowar Husain"<<endl;
+    cout<<"ID: 02422200052101453\n";
+    cout<<"Department of CSE\n";
+    cout<<"Daffodil International University\n";
+   return 0;
+}
